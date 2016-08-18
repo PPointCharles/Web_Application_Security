78d170221273f938b314f1f5550b800b9592501b
YeWang0 <yewang0910@gmail.com>
2016-08-18 15:19:16 -0400

    remove temp files

--- class7/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-    include "hash_methods.php";
-
-    $salt ='salt'; //openssl_random_pseudo_bytes(40, $was_strong);
-    $pwd ='pass123';
-    $r=2;
-    
-    echo hash_m3($pwd,$salt,$r);
-?>
--- project2/web/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-    $_SESSION['ip']=null;
-    $_SESSION["logged_in"] = false;
-    $_SESSION["username"] = null;
-    echo '{"status" : "success"}';
-?>
